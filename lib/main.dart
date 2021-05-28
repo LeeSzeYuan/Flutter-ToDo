c3b32c9e1bbcd63b913e332dd8fd524f65b2d7b8
Sze Yuan <55387911+LeeSzeYuan@users.noreply.github.com>
2021-05-28 23:19:40 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo/screens/todoList.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: todoList(),
+    );
+  }
+}
+
